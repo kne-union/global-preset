1dc5e58f7d5fc1be50a21f639cd203d96d9a8036
zhipenglin <sunandmoon001@163.com>
2023-08-28 16:54:02 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const Lib = () => {
-    return 'unleash creativity';
+import {createContext, useContext, useRef} from 'react';
+
+export const context = createContext({});
+
+const {Provider: ReactProvider} = context;
+
+const Provider = ({value, children}) => {
+    const preset = useRef(value);
+    preset.current = value;
+    return <ReactProvider value={preset}>
+        {children}
+    </ReactProvider>
 };
 
-export default Lib;
+const usePreset = () => {
+    const preset = useContext(context);
+    return preset.current;
+};
+
+export {Provider, usePreset};
